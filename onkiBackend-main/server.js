d8df5909ba1ad04fae485e1c45f85a4733f9430c
yubinee12 <soongsilyubin@gmail.com>
2024-10-04 19:45:30 +0900

    “a”

--- onkiBackend-main/server.js
+++ onkiBackend-main/server.js
@@ -1,21 +1,18 @@
 // 필요한 모듈 불러오기
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const sha = require('js-sha256').sha256;
+const express=require('express');
+const path = require('path');
+const app=express();
+
 const ejs = require('ejs');
-let session = require('express-session');
-app.use(session({
-    secret : 'dlsfjekjfleij235m',
-    resave : false,
-    saveUninitialized : true
-}))
+
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs')
-app.use("/public", express.static("public"));
+app.set('views', path.join(__dirname, 'views'));
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 // const dotenv = require('dotenv').config(); // 환경변수
 const port = 3000;
 const mongoclient = require('mongodb').MongoClient;
@@ -36,58 +33,7 @@ mongoclient.connect(url)
     });
 
 
-// 원격 템플릿 로더 함수 1
-// async function remoteTemplateLoader(templateName) {
-//     const githubRawUrl = `https://raw.githubusercontent.com/onkiproject/onkiFrontend/master/roots/${templateName}.ejs`;
-//     try {
-//         const response = await axios.get(githubRawUrl);
-//         return response.data;
-//     } catch (error) {
-//         console.error(`템플릿 로딩 오류: ${error}`);
-//         return null;
-//     }
-// }
 
-
-//원격 템플릿 로더 함수 2
-async function remoteTemplateLoader(templateName) {
-    const githubRawUrl = `https://raw.githubusercontent.com/onkiproject/onkiFrontend/master/roots/${templateName}.ejs`;
-    console.log(`템플릿 URL: ${githubRawUrl}`);
-    try {
-        const response = await axios.get(githubRawUrl, { timeout: 5000 });
-        if (response.status === 200) {
-            console.log('템플릿 로딩 성공');
-            return response.data;
-        } else {
-            console.error(`템플릿 로딩 실패. 상태 코드: ${response.status}`);
-            return null;
-        }
-    } catch (error) {
-        console.error(`템플릿 로딩 오류: ${error.message}`);
-        return null;
-    }
-}
-
-// Express 뷰 엔진 설정
-app.engine('ejs', async (filePath, options, callback) => {
-    try {
-        const templateName = filePath.split('/').pop().split('.')[0];
-        console.log(`Loading template: ${templateName}`);
-
-        const template = await remoteTemplateLoader(templateName);
-
-        if (template) {
-            const rendered = ejs.render(template, options);
-            return callback(null, rendered);
-        } else {
-            return callback(new Error('템플릿을 로드할 수 없습니다.'));
-        }
-    } catch (error) {
-        return callback(error);
-    }
-});
-
-// 라우트 
-app.get('/newdiary', (req, res) => {
-    res.render('newdiary1', { title: '새 일기 작성' });
-});
+    app.get('/newdiary1', function(req, res){
+        res.render('newdiary1.ejs')
+    })
